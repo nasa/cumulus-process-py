1c2eff4b11342f7ea52615de6fe99bc502432f57
Matthew Hanson <matt.a.hanson@gmail.com>
2017-06-21 18:23:08 -0400

    quiet external loggers

---
+++ cumulus/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+# quiet these loggers
+logging.getLogger('boto3').setLevel(logging.CRITICAL)
+logging.getLogger('botocore').setLevel(logging.CRITICAL)
+logging.getLogger('nose').setLevel(logging.CRITICAL)
+logging.getLogger('s3transfer').setLevel(logging.CRITICAL)
+logging.getLogger('dicttoxml').setLevel(logging.CRITICAL)
+
+
+from cumulus.granule import Granule
