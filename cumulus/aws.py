992156fec948c42602ab8552e7170fbb8163bdb8
Matthew Hanson <matt.a.hanson@gmail.com>
2017-06-22 01:19:36 -0400

    activity service, lambda handler, and run functions added to aws module

--- cumulus/aws.py
+++ cumulus/aws.py
@@ -1,34 +1,54 @@
 import os
 import json
 import cumulus.s3 as s3
+from cumulus.payload import parse_payload
 import boto3
+import traceback
+from botocore.client import Config
+from botocore.vendored.requests.exceptions import ReadTimeout
 from cumulus.loggers import getLogger
 
 logger = getLogger(__name__)
 
 
-
-
-
-def process(payload):
-    gran = granule_from_payload(payload)
-    gran.process()
-    #payload['granuleRecord']['files'][f]['stagingFile'] = uri
-
-
-
 def lambda_handler(payload):
     """ Handler for AWS Lambda function """
-    pass
+    run(payload)
 
 
-def run_task():
+def run(cls, payload, path='/tmp', s3path='', noclean=False, **kwargs):
+    """ Run this payload with the given Granule class """
+    pl = parse_payload(payload)
+    granule = cls(payload['filenames'], gid=pl['gid'], collection=pl['collection'],
+                  path=path, s3path=s3path)
+    granule.run(noclean=noclean)
+
+
+def get_and_run_task(cls, sfn, arn):
     """ Get and run a single task as part of an activity """
+    logger.info('qury for task')
+    try:
+        task = sfn.get_activity_task(activityArn=arn, workerName=__name__)
+    except ReadTimeout:
+        logger.warning('Activity read timed out. Trying again.')
+        return
+    try:
+        payload = json.loads(task['input'])
+        # run job
+        result = run(cls, payload)
+        # return sucess with result
+        sfn.send_task_success(taskToken=task['taskToken'], output=json.dumps({'result': result}))
+    except Exception as e:
+        tb = traceback.format_exc()
+        sfn.send_task_failure(taskToken=task['taskToken'], error=str(e), cause=tb)
 
 
-def activity():
+def activity(cls):
     """ An activity service for use with AWS Step Functions """
-    pass
+    arn = os.getenv('ACTIVITY_ARN')
+    sfn = boto3.client('stepfunctions', config=Config(read_timeout=70))
+    while True:
+        get_and_run_task(cls, sfn, arn)
 
 
 def next(self, payload, lambda_name):
