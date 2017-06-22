5dd72df3d1263f0faab1a018d58109de4e0aae4c
Matthew Hanson <matt.a.hanson@gmail.com>
2017-06-21 23:25:25 -0400

    working payload module and tests

--- cumulus/payload.py
+++ cumulus/payload.py
@@ -3,32 +3,37 @@ import json
 import cumulus.s3 as s3
 
 
-def check_payload(self):
+def check_payload(payload):
     """ Test validity of payload """
     try:
-        assert('granuleRecord' in self.payload)
-        assert('recipe' in self.payload['granuleRecord'])
-        assert('files' in self.payload['granuleRecord'])
-        assert('processStep' in self.payload['granuleRecord']['recipe'])
-        assert('config' in self.payload['granuleRecord']['recipe']['processStep'])
+        assert('granuleRecord' in payload)
+        assert('recipe' in payload['granuleRecord'])
+        assert('files' in payload['granuleRecord'])
+        assert('processStep' in payload['granuleRecord']['recipe'])
+        assert('config' in payload['granuleRecord']['recipe']['processStep'])
     except:
         raise ValueError("Invalid payload")
 
 
-def process_payload(payload):
+def parse_payload(payload):
     """ Process an input payload that is a file, a json string, or s3 location """
     if isinstance(payload, str):
         if payload[0:5] == 's3://':
             # s3 location
             payload = s3.download_json(payload)
         else:
-            if not os.path.exists(payload):
-                raise ValueError("Payload file %s does not exist" % payload)
-            with open(payload, 'r') as f:
-                payload = json.loads(f.read())
+            if os.path.exists(payload):
+                with open(payload, 'r') as f:
+                    payload = json.loads(f.read())
+            else:
+                try:
+                    payload = json.loads(payload)
+                except:
+                    raise ValueError("Invalid payload: %s" % payload)
     check_payload(payload)
     record = payload['granuleRecord']
     filenames = [record['files'][r]['stagingFile'] for r in record['recipe']['processStep']['config']['inputFiles']]
+
     return {
         'gid': payload['granuleRecord']['granuleId'],
         'collection': payload['granuleRecord']['collectionName'],
--- test/test_payload.py
+++ test/test_payload.py
@@ -1,17 +1,48 @@
 import os
+import json
 import unittest
-from cumulus.payload import process_payload
+import cumulus.s3 as s3
+from cumulus.payload import parse_payload
+from nose.tools import raises
 
 
 class TestPayload(unittest.TestCase):
     """ Test utiltiies for publishing data on AWS PDS """
 
-    bucket = 'cumulus-internal'
+    s3path = 's3://cumulus-internal/testing/cumulus-py'
     testdir = os.path.dirname(__file__)
     payload = os.path.join(testdir, 'payload.json')
 
-    def test_parse_payload_file(self):
-        """ Parse test payload """
-        payload = process_payload(self.payload)
+    def test_parse_payload_json(self):
+        """ Parse JSON payload """
+        with open(self.payload, 'r') as f:
+            payload = json.loads(f.read())
+        payload = parse_payload(payload)
         self.assertTrue('gid' in payload)
         self.assertEqual(len(payload['filenames']), 2)
+
+    def test_parse_payload_string(self):
+        """ Parse JSON string payload """
+        with open(self.payload, 'r') as f:
+            payload = f.read()
+        payload = parse_payload(payload)
+        self.assertTrue('gid' in payload)
+        self.assertEqual(len(payload['filenames']), 2)
+
+    def test_parse_payload_file(self):
+        """ Parse test payload file """
+        payload = parse_payload(self.payload)
+        self.assertTrue('gid' in payload)
+        self.assertEqual(len(payload['filenames']), 2)
+
+    def test_parse_payload_from_s3(self):
+        """ Parse test payload from S3 file """
+        uri = s3.upload(self.payload, self.s3path)
+        payload = parse_payload(uri)
+        self.assertTrue('gid' in payload)
+        self.assertEqual(len(payload['filenames']), 2)
+
+    @raises(ValueError)
+    def test_parse_invalid_payload(self):
+        """ Parse invalid payload """
+        parse_payload('nonsensestring')
