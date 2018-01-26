c8726d59837933bac6e29b5674f3df6d36f3ae85
Matthew Hanson <matt.a.hanson@gmail.com>
2018-01-26 17:46:47 -0500

    factored out most Payload functions

--- cumulus/payload.py
+++ cumulus/payload.py
@@ -27,64 +27,6 @@ class Payload(object):
         self.validate(payload)
         self.payload = payload
 
-    @classmethod
-    def validate(cls, payload):
-        """ Test validity of payload """
-        try:
-            assert('resources' in payload)
-            assert('buckets' in payload['resources'])
-            assert('collection' in payload)
-            assert('files' in payload['collection']['meta'])
-            assert('payload' in payload)
-            assert('granules' in payload['payload'])
-        except Exception as e:
-            logger.error(str(e))
-            raise ValueError("Invalid payload")
-
-    def filenames(self):
-        """ Get input filenames """
-        granules = self.payload['payload']['granules']
-        filenames = []
-        for g in granules:
-            filenames.append([f['filename'] for f in g['files']])
-        return filenames
-
-    @property
-    def default_url(self):
-        """ Get default endpoint """
-        default = 'https://cumulus.com'
-        return self.payload['resources'].get('distribution_endpoint', default)
-
-    @property
-    def default_urlpath(self):
-        """ Get default s3 path """
-        return self.payload['collection']['meta'].get('url_path', '')
-
-    @property
-    def urls(self):
-        """ Get dictionary of regex keys with s3 and http urls """
-        files = self.payload['collection']['meta']['files']
-        buckets = self.payload['resources']['buckets']
-        urls = {}
-        for f in files:
-            url_path = f.get('url_path', self.default_urlpath)
-            access = f.get('bucket', 'public')
-            if access == 'public':
-                http = 'http://%s.s3.amazonaws.com' % buckets[access]
-            else:
-                http = self.default_url
-            http = os.path.join(http, url_path)
-            urls[f['regex']] = {
-                'access': access,
-                's3': os.path.join('s3://%s' % buckets[access], url_path),
-                'http': os.path.join(http, url_path)
-            }
-        return urls
-
-    @property
-    def gid_regex(self):
-        return self.payload['collection']['meta'].get('granuleIdExtraction', None)
-
     def add_output_granule(self, gid, granule):
         """ Add output granules to the payload """
         # new files to add
