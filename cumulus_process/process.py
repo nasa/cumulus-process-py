8d8f09b4052681e76daa8ea32e088c6abe38a631
Matthew Hanson <matt.a.hanson@gmail.com>
2018-01-31 19:08:45 -0500

    fix import from rename

--- cumulus_process/process.py
+++ cumulus_process/process.py
@@ -5,7 +5,7 @@ import subprocess
 import logging
 from dicttoxml import dicttoxml
 from xml.dom.minidom import parseString
-import cumulus.s3 as s3
+import cumulus_process.s3 as s3
 from cumulus_process.loggers import getLogger
 from cumulus_process.cli import cli
 from cumulus_process.handlers import activity_handler
