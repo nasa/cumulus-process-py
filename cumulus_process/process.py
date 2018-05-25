93740643aa96981ce97eb91a3e26b760b18e157f
Scisco <scisco7@gmail.com>
2018-05-25 07:26:53 -0400

    add deprecation warnings to methods and properties for future removal

--- cumulus_process/process.py
+++ cumulus_process/process.py
@@ -4,6 +4,7 @@ import re
 import gzip
 import subprocess
 import logging
+import warnings
 from tempfile import mkdtemp
 from dicttoxml import dicttoxml
 from xml.dom.minidom import parseString
@@ -34,11 +35,18 @@ class Process(object):
 
     def process(self):
         """ Process data to produce one or more output granules """
-        return {}
+        NotImplementedError(
+            'This method is not implemented in the base process ' +
+            'class and MUST be implemented in subclasses of process'
+        )
 
     @property
     def gid(self):
         """ Get GID based on regex if provided """
+        warnings.warn(
+            'gid property is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         gid = None
 
         if not isinstance(self.input, list):
@@ -109,10 +117,19 @@ class Process(object):
 
     def fetch_all(self, remote=False):
         """ Download all files in remote_in """
+        warnings.warn(
+            'fetch_all method is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         return {key: self.fetch(key=key, remote=remote) for key in self.input_keys}
 
     def upload_file(self, filename):
         """ Upload a local file to s3 if collection payload provided """
+        warnings.warn(
+            'upload_file method is deprecated and will be removed in the next release. ' +
+            'use helper functions from the helper module instead',
+            DeprecationWarning
+        )
         info = self.get_publish_info(filename)
         if info is None:
             return filename
@@ -131,10 +148,19 @@ class Process(object):
 
     def upload_output_files(self):
         """ Uploads all self.outputs """
+        warnings.warn(
+            'upload_output_files method is deprecated and will be removed in the next release. ' +
+            'use helper functions from the helper module instead',
+            DeprecationWarning
+        )
         return [self.upload_file(f) for f in self.output]
 
     def clean_downloads(self):
         """ Remove input files """
+        warnings.warn(
+            'clean_downloads method is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         self.logger.info('Cleaning downloaded files')
         for f in self.downloads:
             if os.path.exists(f) and f not in self.output:
@@ -142,6 +168,10 @@ class Process(object):
 
     def clean_output(self):
         """ Remove local output files """
+        warnings.warn(
+            'clean_output method is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         for f in self.output:
             if os.path.exists(f):
                 os.remove(f)
@@ -155,19 +185,35 @@ class Process(object):
     # properties to access payload parameters for publishing
     @property
     def buckets(self):
+        warnings.warn(
+            'buckets property is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         return self.config.get('buckets', {})
 
     @property
     def collection(self):
+        warnings.warn(
+            'collection property is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         return self.config.get('collection', {})
 
     @property
     def default_url(self):
         """ Get default endpoint """
+        warnings.warn(
+            'default_url property is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         return self.config.get('distribution_endpoint', 'https://cumulus.com')
 
     def get_publish_info(self, filename):
         """ Get publishing info for this file from the collection metadata """
+        warnings.warn(
+            'get_publish_info method is deprecated and will be removed in the next release',
+            DeprecationWarning
+        )
         files = self.config.get('files_config', [])
         info = None
         count = 0
@@ -195,6 +241,11 @@ class Process(object):
     @classmethod
     def dicttoxml(cls, meta, pretty=False, root='Granule'):
         """ Convert dictionary metadata to XML string """
+        warnings.warn(
+            'dicttoxml method is deprecated and will be removed in the next release. ' +
+            'use helper functions from the helper module instead',
+            DeprecationWarning
+        )
         # for lists, use the singular version of the parent XML name
         singular_key_func = lambda x: x[:-1]
         # convert to XML
@@ -215,6 +266,11 @@ class Process(object):
     @classmethod
     def write_metadata(cls, meta, fout, pretty=False):
         """ Write metadata dictionary as XML file """
+        warnings.warn(
+            'write_metadata method is deprecated and will be removed in the next release. ' +
+            'use helper functions from the helper module instead',
+            DeprecationWarning
+        )
         xml = cls.dicttoxml(meta, pretty=pretty)
         with open(fout, 'w') as f:
             f.write(xml)
@@ -233,6 +289,11 @@ class Process(object):
     @classmethod
     def gunzip(cls, fname, remove=False):
         """ Unzip a file, creating new file """
+        warnings.warn(
+            'gunzip method is deprecated and will be removed in the next release. ' +
+            'use helper functions from the helper module instead',
+            DeprecationWarning
+        )
         f = os.path.splitext(fname)[0]
         with gzip.open(fname, 'rb') as fin:
             with open(f, 'wb') as fout:
@@ -244,6 +305,11 @@ class Process(object):
     @classmethod
     def basename(cls, filename):
         """ Strip path and extension """
+        warnings.warn(
+            'basename method is deprecated and will be removed in the next release. ' +
+            'use helper functions from the helper module instead',
+            DeprecationWarning
+        )
         return os.path.splitext(os.path.basename(filename))[0]
 
     # ## Handlers
